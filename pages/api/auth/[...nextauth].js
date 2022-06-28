e8234f539e9ef8cb51dd482550930ae91d02ade8
phichayut-pak <pakkung1245@gmail.com>
2022-06-29 02:03:47 +0700

    fixed secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -77,6 +77,7 @@ export default NextAuth({
   
       return session
     }
-  }
+  },
+  secret: process.env.NEXTAUTH_SECRET
 })
 
--- pages/api/auth/test.js
+++
@@ -1,7 +0,0 @@
-const handler = (req, res) => {
-  res.status(200).json({
-    message: 'Done'
-  })
-}
-
-export default handler
