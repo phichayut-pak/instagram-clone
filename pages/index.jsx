50c55a822e9f7cf9d1d5a055910c0c9514daa5dd
phichayut-pak <pakkung1245@gmail.com>
2022-06-28 12:47:09 +0700

    fixed path

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import { motion } from 'framer-motion'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
 export default function Home() {
+  const { push } = useRouter()
+  
   return (
     <div className="min-h-screen flex justify-center items-center">
       <motion.div className="text-2xl font-['Poppins'] "
@@ -9,7 +12,7 @@ export default function Home() {
         transition={{ type: 'spring', stiffness: 100, duration: 0.5 }}
         
       >
-        <button className='border-2 py-2 px-4 font-bold' onClick={signIn}> {/* when it's clicked, it'll bring to signin page */}
+        <button className='border-2 py-2 px-4 font-bold' onClick={() => push(`/auth/signin?callbackUrl=${asPath}`)}> {/* when it's clicked, it'll bring to signin page */}
           Click me
         </button>
       </motion.div>
